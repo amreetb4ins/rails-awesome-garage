dec5c476494c5e5223e9811553a789d44642777c
Amreet Bains <amreet8@hotmail.co.uk>
2024-10-03 17:52:38 +0100

    added destroy route

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -13,4 +13,10 @@ class FavouritesController < ApplicationController
       render "cars/show", status: :unprocessable_entity
     end
   end
+
+  def destroy
+   @favourite = Favourite.find(params[:id])
+   @favourite.destroy
+   redirect_to favourites_path, status: :see_other
+  end
 end
